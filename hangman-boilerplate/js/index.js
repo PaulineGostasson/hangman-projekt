0b12b646e229079bed07cf25f0db943f252b91d6
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-01 10:37:04 +0100

    knappar inlagda

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -7,3 +7,39 @@
  document.querySelector('figure').classList.add('legs')
 
  */
+
+const ul = document.querySelector("ul");
+
+const alphabetArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+for (let i = 0; i < alphabetArray.length; i++) {
+  const letterToPush = `<li><button id="button${alphabetArray[i]}">${alphabetArray[i]}</button></li>`;
+  ul.insertAdjacentHTML("beforeend", letterToPush);
+}
